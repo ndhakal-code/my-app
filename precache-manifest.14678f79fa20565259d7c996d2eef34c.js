b5c803e5a0458745dde824ac74dc80614a8c0415
Nishan Dhakal <ndhakal54@outlook.com>
2020-06-25 12:16:46 -0400

    Updates

---
+++ precache-manifest.14678f79fa20565259d7c996d2eef34c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699b69b74a6fb4a311b52f49e68ed6de",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "0ec56114e265e07e1148",
+    "url": "/my-app/static/css/main.94e09bc3.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec56114e265e07e1148",
+    "url": "/my-app/static/js/main.f0d9baef.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
